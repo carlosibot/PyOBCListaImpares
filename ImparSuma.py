7de977bd1094e5cf1c1b8c28bfe0865c908baf8e
carlosibot <106968244+carlosibot@users.noreply.github.com>
2022-08-01 12:46:29 -0400

    update

--- ImparSuma.py
+++ ImparSuma.py
@@ -5,8 +5,5 @@ listaF = filter(lambda x: (x % 2 != 0), lista)
 NumeroImpares = list(listaF)
 print('Lista de numero impares: ', NumeroImpares)
 
-def suma(a, b):
-    return a + b
-
 SumaNumero = functools.reduce(lambda a, b: a + b, NumeroImpares)
 print('Suma de impares es: ', SumaNumero)
